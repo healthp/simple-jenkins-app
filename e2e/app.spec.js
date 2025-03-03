de57f79f40936687389a4ed3e0af5109acb0df60
healthp <healthp@proton.me>
2025-03-03 01:42:30 +0000

    Update app.spec.js with waitForLoadState

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -3,15 +3,17 @@ const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-
+  await page.waitForLoadState('load'); // Ensure page is fully loaded
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Simple Jenkins App/);
 });
 
 test('has Jenkins in the body', async ({ page }) => {
-  //await page.goto('/');
-  await page.waitForSelector('a:has-text("Simple Jenkins App")', { state: 'visible' });
+  await page.goto('/'); // If needed, ensure the page is loaded
+  await page.waitForLoadState('load'); // Ensure page is fully loaded
+  await page.waitForSelector('a:has-text("Simple Jenkins App")', { state: 'visible',timeout: 60000 });
   const isVisible = await page.locator('a:has-text("Simple Jenkins App")').isVisible();
+  console.log('Is element visible:', isVisible); // Log visibility for debugging
   expect(isVisible).toBeTruthy();
 });
 
