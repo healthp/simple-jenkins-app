c9e7f027ac89f3c26ec1e3122156368683f7d1a6
healthp <healthp@proton.me>
2025-03-03 00:56:17 +0000

    update of index.html

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -5,13 +5,13 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Simple Jenkins App/);
+  await expect(page).toHaveTitle('Simple Jenkins App');
 });
 
 test('has Jenkins in the body', async ({ page }) => {
   await page.goto('/');
 
-  const isVisible = await page.locator('a:has-text("Simple Jenkins App")').isVisible();
+  const isVisible = await page.locator('a:has-text("Simple Jenkins")').isVisible();
   expect(isVisible).toBeTruthy();
 });
 
